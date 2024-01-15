e2981f07a93c5ac29df6b53a5eb7339fdacd05c0
Antonino <pagana899@gmail.com>
2024-01-15 23:47:51 +0100

    Esercizio Completo 2

--- Main.java
+++ Main.java
@@ -3,13 +3,26 @@ import java.time.OffsetDateTime;
 public class Main {
     public static void main(String[] args) {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+    }
+
+    public Integer ottieniAnno(OffsetDateTime data) {
         Integer anno = data.getYear();
-        System.out.println("Anno:" + anno);
+        return anno;
+    }
+
+    public String ottieniMese(OffsetDateTime data) {
         String mese = data.getMonth().toString();
-        System.out.println("Mese: " + mese);
+        return mese;
+    }
+
+    public Integer ottieniGiorno(OffsetDateTime data) {
         Integer giorno = data.getDayOfMonth();
-        System.out.println("Giorno: " + giorno);
+        return giorno;
+    }
+
+    public String ottieniGiornoDellaSettimana(OffsetDateTime data) {
         String giornoDellaSettimana = data.getDayOfWeek().toString();
-        System.out.println("Giorno della settimana: " + giornoDellaSettimana);
+        return giornoDellaSettimana;
     }
 }
--- MainTest.java
+++ MainTest.java
@@ -5,35 +5,33 @@ import java.time.OffsetDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
+    Main testing = new Main();
+
     @Test
     void controlloAnno() {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        Integer actual = data.getYear();
-        Integer expected = 2023;
-        assertEquals(expected, actual);
+        Integer result = testing.ottieniAnno(data);
+        assertEquals(2023, result);
     }
 
     @Test
     void controlloMese() {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String actual = data.getMonth().toString();
-        String expected = "MARCH";
-        assertEquals(expected, actual);
+        String result = testing.ottieniMese(data);
+        assertEquals("MARCH", result);
     }
 
     @Test
     void controlloGiorno() {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        Integer actual = data.getDayOfMonth();
-        Integer expected = 01;
-        assertEquals(expected, actual);
+        Integer result = testing.ottieniGiorno(data);
+        assertEquals(01, result);
     }
 
     @Test
     void controlloGiornoDellaSettimana() {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String actual = data.getDayOfWeek().toString();
-        String expected = "WEDNESDAY";
-        assertEquals(expected, actual);
+        String result = testing.ottieniGiornoDellaSettimana(data);
+        assertEquals("WEDNESDAY", result);
     }
 }
